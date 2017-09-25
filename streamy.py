62645faf79b34562fab7b7d8862794d70c8a7435
Tom Ritchford <tom@swirly.com>
2017-09-25 14:48:47 +0200

    Use json.JSONEncoder.raw_decode exclusively

--- streamy.py
+++ streamy.py
@@ -1,4 +1,6 @@
-import io, json, platform, re
+#!/usr/bin/env python
+
+import argparse, json, platform, re, sys
 
 """
 Emit a stream of valid JSON from a seekable file pointer like you'd get from
@@ -7,99 +9,31 @@ open or io.StringIO.
 See https://stackoverflow.com/a/44190177/43839
 """
 
-# Before Python 3.6, you need to parse the error thrown to get the character
-# position. A typical error thrown looks like
+_IS_PY3 = platform.python_version() >= '3'
+_STRING_TYPES = str if _IS_PY3  else (str, unicode)  # noqa: F821
+_PARSE_EXCEPTIONS = platform.python_version() >= '3.4'
+
+# When we get an exception parsing a possibly-incomplete stream of JSON,
+# there are two cases:
 #
-#   ValueError: Extra data: line 1 column 4 - line 1 column 6 (char 3 - 5)
+#   1. we don't have enough data yet to generate a complete JSON object, or
+#   2. the data so far is already malformed, and requesting more won't help.
+#
+# json's exception doesn't give us enough information here so we have to parse
+# the exception text.
+#
+# The rule that evolved to recognize case 1:
 
-_MATCH_EXTRA_DATA_ERROR = re.compile(r'Extra data: .*char (\d+).*').match
+# "Not enough data" exception messages that start with 'Expecting', and the
+# character position that they identify has to be right after the last character
+# in the buffer.
 
+# "Unterminated string" messages of any type.
 
-if platform.python_version_tuple()[0] == '3':
-    def to_stream(fp):
-        return io.StringIO(fp) if isinstance(fp, str) else fp
-
-else:
-    def to_stream(fp):
-        if isinstance(fp, unicode):  # noqa: F821
-            return io.StringIO(fp)
-
-        if isinstance(fp, str):
-            return io.StringIO(unicode(fp))  # noqa: F821
-
-        return fp
+_MATCH_EXPECTING_EXCEPTION = re.compile(r'Expecting .*: .*char (\d+).*').match
 
 
-def _get_end_pos(e):
-    try:
-        return e.pos
-    except AttributeError:
-        pass
-
-    try:
-        match = _MATCH_EXTRA_DATA_ERROR(e.args[0])
-        return int(match.group(1))
-    except:
-        pass
-
-
-def _stream_with_seek(fp, kwds):
-    start_pos = 0
-    while True:
-        # Skip whitespace
-        while True:
-            ch = fp.read(1)
-            if not ch:
-                return
-            if not ch.isspace():
-                fp.seek(start_pos)
-                break
-            start_pos += 1
-
-        try:
-            yield json.load(fp, **kwds)
-            return
-        except ValueError as e:
-            end_pos = _get_end_pos(e)
-            if end_pos is None:
-                raise
-
-            fp.seek(start_pos)
-            json_str = fp.read(end_pos)
-            obj = json.loads(json_str, **kwds)
-            start_pos += end_pos
-            yield obj
-
-
-def _stream_without_seek(fp, kwds):
-    buf = ''
-    # XXX: alternatively could read a limited size buffer
-    for l in fp:
-        buf += l
-        while buf:
-            try:
-                obj = json.loads(buf, **kwds)
-            except ValueError as exc:
-                end_pos = _get_end_pos(exc)
-                if end_pos is None:
-                    if (not exc.msg.startswith('Expecting value') or
-                            buf.strip()):
-                        raise
-
-                    # buf is all whitespace
-                    buf = ''
-                    continue
-
-                obj = json.loads(buf[:exc.pos])
-                buf = buf[exc.pos:]
-            else:
-                buf = ''
-            yield obj
-    if buf:
-        raise ValueError('Extra data!')
-
-
-def stream(fp, json_lines=False, **kwds):
+def stream(fp, chunk_size=0, max_message_size=0, **kwds):
     """
     A function generating a stream of valid JSON objects.
 
@@ -109,19 +43,83 @@ def stream(fp, json_lines=False, **kwds):
         json_lines: if true, each line holds at most one JSON expression.
         kwds: keywords to pass to json.load or json.loads.
     """
-    fp = to_stream(fp)
+    def yield_chunks():
+        while True:
+            chunk = fp.read(chunk_size)
+            if not chunk:
+                return
+            yield chunk
 
-    if json_lines:
-        for i in fp:
-            i = i.strip()
-            if i:
-                yield json.loads(i, **kwds)
-        return
-
-    if hasattr(fp, 'seek'):
-        func = _stream_with_seek
+    if isinstance(fp, _STRING_TYPES):
+        chunks = [fp]
+    elif chunk_size:
+        chunks = yield_chunks()
     else:
-        func = _stream_without_seek
+        chunks = fp
 
-    for obj in func(fp, kwds):
-        yield obj
+    decoder = json.JSONDecoder(**kwds)
+    unread = ''
+    for chunk in chunks:
+        unread = ((unread + chunk) if unread else chunk).lstrip()
+        if max_message_size and len(unread) > max_message_size:
+            raise ValueError('Message size exceeded max_message_size')
+
+        while unread:
+            try:
+                data, index = decoder.raw_decode(unread)
+            except ValueError as e:
+                if not _PARSE_EXCEPTIONS:
+                    # In Python 2, we just don't get enough information in
+                    # the exception to figure out if we're in case 2.
+                    break
+                match = _MATCH_EXPECTING_EXCEPTION(e.args[0])
+                if match and int(match.group(1)) >= len(unread) - 2:
+                    break
+                if e.args[0].startswith('Unterminated string'):
+                    break
+
+                # We're in case 2.
+                raise
+            else:
+                yield data
+                unread = unread[index:].strip()
+
+    if unread:
+        raise ValueError('Extra text at end of stream')
+
+
+def _write_input_to_output(args):
+    objects = 0
+    try:
+        with (open(args.output, 'w') if args.output else sys.stdout) as output:
+            with (open(args.input) if args.input else sys.stdin) as input:
+                for i in stream(input, args.chunk_size, args.max_message_size):
+                    json.dump(i, output)
+                    output.write('\n')
+                    objects += 1
+    finally:
+        sys.stderr.write('JSON object writes: %s\n' % objects)
+
+
+def _parse_args():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument(
+        'input', nargs='?', help='Input filename.  If none, use stdin')
+
+    parser.add_argument(
+        'output', nargs='?', help='Output filename.  If none, use stdout')
+
+    parser.add_argument(
+        '-c', '--chunk_size', default=0, type=int,
+        help='Size of chunks to read from stream.  If 0, use readline.')
+
+    parser.add_argument(
+        '-m', '--max_message_size', default=0, type=int,
+        help='Maximum byte size of valid messages or 0 for no maximum.')
+
+    return parser.parse_args(sys.argv[1:])
+
+
+if __name__ == '__main__':
+    _write_input_to_output(_parse_args())
--- test_streamy.py
+++ test_streamy.py
@@ -1,14 +1,10 @@
 import json, streamy, unittest, io
 
+FILE_EXPECTED = [{'a': 'b'}, {'c': 'd'}, [1, {'ab': 'c', 'd': 2}]]
+
 
 class StreamyTest(unittest.TestCase):
     def assert_stream(self, s, *expected):
-        self.assertEqual(list(streamy.stream(s)), list(expected))
-        s = streamy.to_stream(s)
-        try:
-            delattr(s, 'seek')
-        except:
-            return
         self.assertEqual(list(streamy.stream(s)), list(expected))
 
     def stream_fails(self, s):
@@ -38,7 +34,16 @@ class StreamyTest(unittest.TestCase):
 
     def test_file(self):
         with open('test_file.txt') as fp:
-            self.assert_stream(fp, {'a': 'b'}, {'c': 'd'})
+            self.assert_stream(fp, *FILE_EXPECTED)
+
+    def test_carriage_return(self):
+        self.assert_stream('[\n1]', [1])
+
+    def test_chunk_size(self):
+        for i in range(16):
+            with open('test_file.txt') as fp:
+                result = list(streamy.stream(fp, chunk_size=1+i))
+                self.assertEqual(result, FILE_EXPECTED)
 
     def test_error(self):
         self.stream_fails(']')
